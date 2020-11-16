6325ff57797e539ebebff435f2dbaf3066c172f9
Aalo-Avi <71498371+Aalo-Avi@users.noreply.github.com>
2020-11-16 18:19:44 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,23 @@
+class Plinko
+{
+    constructor(x,y)
+    {
+        var options={
+            isStatic:true
+        }
+        this.x = x;
+        this.y = y;
+        this.r = 10;
+        this.body = Bodies.rectangle(x,y,this.r,options);
+        World.add(world,this.body);
+    }
+    display()
+    {
+        var pos = this.body.position;
+
+        ellipseMode(RADIUS);
+        noStroke();
+        fill("white");
+        ellipse(pos.x,pos.y,this.r,this.r);
+    }
+}
